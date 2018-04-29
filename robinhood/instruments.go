d90653725e769d978c43b5b2f88fa6294d156e4a
Jordan Day <jordanday444@gmail.com>
2018-04-28 22:55:54 -0500

    Implement InstrumentByID

--- robinhood/instruments.go
+++ robinhood/instruments.go
@@ -34,3 +34,28 @@ func (c *Client) InstrumentBySymbol(tickerSymbol string) (*Instrument, error) {
 
 	return instruments.Instruments[0], nil
 }
+
+// InstrumentByID gets instrument data by ID
+func (c *Client) InstrumentByID(id string) (*Instrument, error) {
+	req, err := http.NewRequest("GET", fmt.Sprintf("%s%s%s/", baseURL, "/instruments/", id), nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Add("Accept", "application/json")
+
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	content, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	instrument := &Instrument{}
+	json.Unmarshal(content, instrument)
+
+	return instrument, nil
+}
